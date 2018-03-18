04bb4a0e3bd658dfabaf9c0d9175a7189194fec5
krothenbaum <krothenbaum@gmail.com>
2018-03-17 21:27:47 -0700

    Changed image to s3 bucket

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const aws = require('aws-sdk');
 const multer = require('multer');
+const multerS3 = require('multer-s3');
 // const mongoose = require('mongoose');
 const Image = require('../models/image.model');
 
-
 const router = express.Router();
 
-
-const storage = multer.diskStorage ({
-  destination: (req, file, cb) => {
-    cb(null, './public/images');
-  },
-  filename: (reg, file, cb) => {
-    const fileExtension = file.originalname.split('.').pop();
-    cb(null, file.fieldname + '-' + Date.now() + '.'  + fileExtension);
-  }
+aws.config.update({
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY,
+  region: process.env.AWS_REGION
 })
+const s3 = new aws.S3();
+
+// const storage = multer.diskStorage ({
+//   destination: (req, file, cb) => {
+//     cb(null, './public/images');
+//   },
+//   filename: (reg, file, cb) => {
+//     const fileExtension = file.originalname.split('.').pop();
+//     cb(null, file.fieldname + '-' + Date.now() + '.'  + fileExtension);
+//   }
+// })
 
 const imageFilter = (req, file, cb) => {
   // accept image only
@@ -25,7 +32,20 @@ const imageFilter = (req, file, cb) => {
   cb(null, true);
 };
 
-const upload = multer({ storage: storage, fileFilter: imageFilter });
+// const upload = multer({ storage: storage, fileFilter: imageFilter });
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: process.env.AWS_BUCKET,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      console.log(file);
+      const fileExtension = file.originalname.split('.').pop();
+      cb(null, file.fieldname + '-' + Date.now().toString() + '.'  + fileExtension);
+    }
+  }),
+  fileFilter: imageFilter
+});
 
 router.get('/status', (req, res) => res.send('OK'));
 
@@ -40,9 +60,9 @@ router.get('/form', (req, res) => {
 
 router.post('/upload', upload.single('image'), async (req,res) => {
   // res.status(200).send('file uploaded');
-  // console.log(req.file);
+  console.log(req.file);
   try {
-    const image = new Image({ fileName: req.file.filename});
+    const image = new Image({ fileName: req.file.key});
     const savedImage = await image.save();
     res.status(200).json(savedImage);
   } catch (err) {
